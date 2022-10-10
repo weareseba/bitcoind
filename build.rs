0d84df8a1c36b3813b5e41d057b756ea6308fd2e
Richard Ulrich <richard.ulrich@seba.swiss>
2022-10-10 16:36:20 +0200

    fetching binaries through a proxy

--- build.rs
+++ build.rs
@@ -71,10 +71,20 @@ fn main() {
             "https://bitcoincore.org/bin/bitcoin-core-{}/{}",
             VERSION, download_filename
         );
-        let mut downloaded_bytes = Vec::new();
 
-        let _size = ureq::get(&url)
+        let mut downloaded_bytes = Vec::new();
+        let http_proxy = std::env::var("HTTPS_PROXY").or_else(|_| std::env::var("HTTP_PROXY"));
+        let agent = if let Ok(proxy) = http_proxy {
+            let proxy = ureq::Proxy::new(proxy).unwrap();
+            ureq::AgentBuilder::new().proxy(proxy).build()
+        } else {
+            ureq::AgentBuilder::new().build()
+        };
+
+        let _size = agent
+            .get(&url)
             .call()
+            .unwrap()
             .into_reader()
             .read_to_end(&mut downloaded_bytes)
             .unwrap();
